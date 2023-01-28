034ab20505e78e4ca5f28c977e0cb158d8e0e6a3
ALTITUDE DEV <egamestudio@outlook.com>
2023-01-28 22:02:23 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 game 		 'rdr3'
 fx_version 	 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-author 	     'discord.gg/Nq4nExYgfp'
-description  'STORES SYSTEM BY Shepard#1395'
-version 	 '1.0.0'
+author 	     'ALTITUDE-DEV.COM Shepard#1395'
+description  'Standalone HungerGames MiniGame'
+version 	 '0.0.1'
 
 client_scripts {
     'client.lua'
